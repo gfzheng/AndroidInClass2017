2578e58d79901405652f72aa759120b16588bafc
gfzheng <gfzheng@gmail.com>
2017-09-19 15:53:08 +0800

    add demo jpg

--- app/src/main/java/com/guifeng/androidinclass2017/MainActivity.java
+++ app/src/main/java/com/guifeng/androidinclass2017/MainActivity.java
@@ -2,7 +2,7 @@ package com.guifeng.androidinclass2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button b;
     }
 }
